6f3ca0539c49d77b345aa4ac9d02ddc8792a54aa
dana_dobkin <dana.dobkin03@gmail.com>
2024-03-24 12:31:09 +0200

    installation playwright, jest execution

---
+++ test/launchBrowser.test.ts
@@ -0,0 +1,14 @@
+import { chromium } from "playwright";
+
+describe('Launch Browser', () => {
+
+    test('open Letcode', async () => {
+        const browser = await chromium.launch({
+            headless: false
+        });
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto('https://Letcode.in/');
+        await browser.close();
+    });
+});
