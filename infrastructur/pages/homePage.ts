cf594addb65f2bdba31c47ddd6dae275c51f3a10
dana_dobkin <dana.dobkin03@gmail.com>
2024-03-24 23:05:19 +0200

    naming

--- infrastructur/pages/homePage.ts
+++ infrastructur/pages/homePage.ts
@@ -1,5 +1,6 @@
 import { Locator, Page } from "@playwright/test";
 import { BasePage } from "./BasePage";
+import { ProductsCatalog } from "../Layouts/ProductsCatalog";
 
 const locators = {
     categories: '//img[contains(@id, categoryimage)]',
@@ -7,10 +8,12 @@ const locators = {
 
 export class HomePage extends BasePage {
     private categories: Locator;
+    private productsCatalog: ProductsCatalog;
 
     constructor(page: Page) {
         super(page);
         this.categories = page.locator(locators.categories);
+        this.productsCatalog = new ProductsCatalog(this.page);
     }
 
     public async getCategoryItem(categoryName: string) {
@@ -19,5 +22,9 @@ export class HomePage extends BasePage {
 
     public async searchForProduct(productName: string) {
         await this.topBar.searchBar.searchForProduct(productName);
+    }
+
+    public async getFirstItem() {
+        await this.productsCatalog.getFirstItem();
     }
 }
